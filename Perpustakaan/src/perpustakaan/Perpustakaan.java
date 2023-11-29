d171950ee77894aa39311ab2061f9b344938d48a
nadhif21 <112993038+nadhif21@users.noreply.github.com>
2023-11-29 14:39:50 +0700

    Update Perpustakaan.java

--- Perpustakaan/src/perpustakaan/Perpustakaan.java
+++ Perpustakaan/src/perpustakaan/Perpustakaan.java
@@ -17,45 +17,33 @@ public class Perpustakaan {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Membuat objek Admin
         Admin admin = new Admin(1, "Admin Perpustakaan");
 
-        // Membuat beberapa objek Buku
-        Buku buku1 = new Buku("1234567890", "Judul Buku 1", "Pengarang 1", true);
-        Buku buku2 = new Buku("0987654321", "Judul Buku 2", "Pengarang 2", true);
+        Buku buku1 = new Buku("234", "Harry Portal", "JK ROLLER", true);
+        Buku buku2 = new Buku("232", "Lord of The jack", "Dobby", true);
 
-        // Menambahkan buku ke daftar buku admin
         admin.addBuku(buku1);
         admin.addBuku(buku2);
 
-        // Membuat objek AnggotaPerpustakaan
-        AnggotaPerpustakaan anggota = new AnggotaPerpustakaan(101, "Nama Anggota", "Alamat Anggota");
+        AnggotaPerpustakaan anggota = new AnggotaPerpustakaan(101, "Ahlan Sayyid", "Kobum");
 
-        // Menambahkan anggota ke daftar anggota admin
         admin.addAnggota(anggota);
 
-        // Membuat objek TransaksiPeminjaman
-        Date tanggalPeminjaman = new Date();
-        Date tanggalPengembalian = new Date(tanggalPeminjaman.getTime() + (1000 * 60 * 60 * 24 * 7)); // Contoh pengembalian seminggu setelah peminjaman
+        Date tanggalPeminjaman = new Date(2023,11,28);
+        Date tanggalPengembalian = new Date(2023,12,1); 
         TransaksiPeminjaman transaksiPeminjaman = new TransaksiPeminjaman(tanggalPeminjaman, tanggalPengembalian, anggota);
 
-        // Menambahkan transaksi peminjaman ke daftar transaksi peminjaman admin
         admin.setTransaksi_peminjaman(transaksiPeminjaman);
 
-        // Meminjam buku oleh anggota
         anggota.pinjamBuku(buku1, transaksiPeminjaman);
 
-        // Mengembalikan buku oleh anggota
         TransaksiPengembalian transaksiPengembalian = new TransaksiPengembalian(transaksiPeminjaman);
         anggota.kembalikanBuku(buku1, transaksiPengembalian);
 
-        // Mencari buku oleh anggota
-        anggota.cariBuku("Judul Buku 2", admin);
+        anggota.cariBuku("Lord of The jack", admin);
 
-        // Membuat objek Notifikasi
         Notifikasi notifikasi = new Notifikasi(transaksiPeminjaman);
 
-        // Menampilkan pesan notifikasi
         notifikasi.pesan();
     }
     
